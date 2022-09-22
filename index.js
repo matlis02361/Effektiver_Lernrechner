83c34cc70d2fdc48bcfcd2f545e27ba31cac997d
Mateusz <matlis02361@gmail.com>
2022-09-22 10:02:08 +0200

    test branch

--- index.js
+++ index.js
@@ -4,34 +4,16 @@ const parameter = {
   theorieZeit: 0,
   konzentration: 100,
   lernEffizienz: 0,
-
-  /* TEST  */
   praxisToLernnZeit: 0,
 };
-/* ................... Theorie....................................... */
-/* theorieRechner = (zeit) => {
-  parameter.theorieZeit += zeit;
-  if (zeit >= 25) {
-    for (let index = 0; index < zeit; index += 5) {
-      parameter.konzentration -= 5;
-    }
-  }
-  if (zeit >= 25 && zeit <= 30 ) {
-    for (let index = 45; index < zeit; index += 5) {
-        parameter.konzentration -= 6;
-      }
-  } else if (zeit > 30 && zeit <= 45 && parameter.konzentration > 0) {
-    parameter.konzentration -= 15;
-  } else if (zeit > 45 && parameter.konzentration > 0) {
-    parameter.konzentration -= 15;
-    for (let index = 45; index < zeit; index += 5) {
-      parameter.konzentration -= 5;
-    }
-  }
-}; */
 
-theorieRechner = (zeit) => {
-  parameter.theorieZeit += zeit;
+lernRechner = (zeit, Tätigkeit) => {
+  if (Tätigkeit === "Theorie") {
+    parameter.theorieZeit += zeit;
+  }
+  if (Tätigkeit === "Praxis") {
+    parameter.praxisZeit += zeit;
+  }
 
   for (let index = 0; index < zeit; index += 5) {
     if (index >= 25) {
@@ -47,27 +29,14 @@ theorieRechner = (zeit) => {
       parameter.konzentration -= 0.03 * parameter.konzentration;
     }
   }
+  /* ----------------------------------------------------------- */
+  let LernnZeit = parameter.theorieZeit + parameter.praxisZeit;
+  parameter.praxisToLernnZeit = parameter.praxisZeit / LernnZeit;
+
+  parameter.lernEffizienz =
+    100 - Math.abs(0.8 - parameter.praxisToLernnZeit) * parameter.konzentration;
 };
 
-/* ................... Praxis....................................... */
-praxisRechner = (zeit) => {
-  parameter.praxisZeit += zeit;
-
-  for (let index = 0; index < zeit; index += 15) {
-    if (index >= 25) {
-      parameter.konzentration -= 0.01 * parameter.konzentration;
-    }
-    if (index >= 25 && index <= 30) {
-      parameter.konzentration -= 0.015 * parameter.konzentration;
-    }
-    if (index > 30 && index <= 45) {
-      parameter.konzentration -= 0.02 * parameter.konzentration;
-    }
-    if (index > 45 && parameter.konzentration >= 0) {
-      parameter.konzentration -= 0.03 * parameter.konzentration;
-    }
-  }
-};
 /* ................... Pause....................................... */
 pauseRechner = (zeit) => {
   pause += zeit;
@@ -81,48 +50,7 @@ pauseRechner = (zeit) => {
   }
 };
 
-lernRechner = () => {
-  let LernnZeit = parameter.theorieZeit + parameter.praxisZeit;
-  parameter.praxisToLernnZeit = parameter.praxisZeit / LernnZeit;
-
-  parameter.lernEffizienz =
-    100 - Math.abs(0.8 - parameter.praxisToLernnZeit) * parameter.konzentration;
-};
-
-//lernRechner(zeit,Tätigkeit)
-//             45, Theorie
-
-theorieRechner(80);
-praxisRechner(20);
-lernRechner();
+lernRechner(45, "Theorie");
 console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
-console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
-console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
-console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
-console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
-console.log(parameter);
-
-theorieRechner(20);
-praxisRechner(80);
-lernRechner();
+lernRechner(45, "Praxis");
 console.log(parameter);
