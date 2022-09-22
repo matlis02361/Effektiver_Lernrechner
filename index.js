79cb40c790c8a27dd050288323868806edfd1ef7
Mateusz <matlis02361@gmail.com>
2022-09-22 09:48:15 +0200

    branch

--- index.js
+++ index.js
@@ -9,38 +9,63 @@ const parameter = {
   praxisToLernnZeit: 0,
 };
 /* ................... Theorie....................................... */
+/* theorieRechner = (zeit) => {
+  parameter.theorieZeit += zeit;
+  if (zeit >= 25) {
+    for (let index = 0; index < zeit; index += 5) {
+      parameter.konzentration -= 5;
+    }
+  }
+  if (zeit >= 25 && zeit <= 30 ) {
+    for (let index = 45; index < zeit; index += 5) {
+        parameter.konzentration -= 6;
+      }
+  } else if (zeit > 30 && zeit <= 45 && parameter.konzentration > 0) {
+    parameter.konzentration -= 15;
+  } else if (zeit > 45 && parameter.konzentration > 0) {
+    parameter.konzentration -= 15;
+    for (let index = 45; index < zeit; index += 5) {
+      parameter.konzentration -= 5;
+    }
+  }
+}; */
+
 theorieRechner = (zeit) => {
   parameter.theorieZeit += zeit;
 
-  if (zeit <= 25) {
-    parameter.konzentration -= 0;
-    // parameter.lernEffizienz = 99;
-  } else if (zeit <= 30) {
-    parameter.konzentration -= 0;
-    // parameter.lernEffizienz = 100;
-  } else if (zeit <= 45) {
-    parameter.konzentration -= 1;
-    // parameter.lernEffizienz = 95;
-  } else if (zeit >= 45) {
-    for (let index = 45; index < zeit; index += 5) {
-      parameter.konzentration -= 5;
-      //  parameter.lernEffizienz -= 5;
+  for (let index = 0; index < zeit; index += 5) {
+    if (index >= 25) {
+      parameter.konzentration -= 0.01 * parameter.konzentration;
+    }
+    if (index >= 25 && index <= 30) {
+      parameter.konzentration -= 0.015 * parameter.konzentration;
+    }
+    if (index > 30 && index <= 45) {
+      parameter.konzentration -= 0.02 * parameter.konzentration;
+    }
+    if (index > 45 && parameter.konzentration >= 0) {
+      parameter.konzentration -= 0.03 * parameter.konzentration;
     }
   }
 };
+
 /* ................... Praxis....................................... */
 praxisRechner = (zeit) => {
   parameter.praxisZeit += zeit;
 
-  if (zeit <= 25) {
-    parameter.konzentration -= 0;
-    // parameter.lernEffizienz = 99;
-  } else if (zeit <= 30) {
-    parameter.konzentration -= 0;
-    // parameter.lernEffizienz = 100;
-  } else if (zeit <= 45) {
-    parameter.konzentration -= 1;
-
+  for (let index = 0; index < zeit; index += 15) {
+    if (index >= 25) {
+      parameter.konzentration -= 0.01 * parameter.konzentration;
+    }
+    if (index >= 25 && index <= 30) {
+      parameter.konzentration -= 0.015 * parameter.konzentration;
+    }
+    if (index > 30 && index <= 45) {
+      parameter.konzentration -= 0.02 * parameter.konzentration;
+    }
+    if (index > 45 && parameter.konzentration >= 0) {
+      parameter.konzentration -= 0.03 * parameter.konzentration;
+    }
   }
 };
 /* ................... Pause....................................... */
@@ -57,25 +82,47 @@ pauseRechner = (zeit) => {
 };
 
 lernRechner = () => {
-
   let LernnZeit = parameter.theorieZeit + parameter.praxisZeit;
   parameter.praxisToLernnZeit = parameter.praxisZeit / LernnZeit;
-  if (parameter.praxisToLernnZeit >= 0.75 &&  parameter.praxisToLernnZeit <= 0.85){
- parameter.lernEffizienz = 100
-  } else if (parameter.praxisToLernnZeit < 0.75 || parameter.praxisToLernnZeit > 0.85){
-  parameter.lernEffizienz = 100 - (Math.abs(0.8 - parameter.praxisToLernnZeit))*parameter.konzentration }
+
+  parameter.lernEffizienz =
+    100 - Math.abs(0.8 - parameter.praxisToLernnZeit) * parameter.konzentration;
 };
 
 //lernRechner(zeit,Tätigkeit)
 //             45, Theorie
 
-theorieRechner(1)
-praxisRechner(100)
+theorieRechner(80);
+praxisRechner(20);
 lernRechner();
 console.log(parameter);
 
-theorieRechner(45)
-praxisRechner(80)
+theorieRechner(20);
+praxisRechner(80);
 lernRechner();
 console.log(parameter);
 
+theorieRechner(20);
+praxisRechner(80);
+lernRechner();
+console.log(parameter);
+
+theorieRechner(20);
+praxisRechner(80);
+lernRechner();
+console.log(parameter);
+
+theorieRechner(20);
+praxisRechner(80);
+lernRechner();
+console.log(parameter);
+
+theorieRechner(20);
+praxisRechner(80);
+lernRechner();
+console.log(parameter);
+
+theorieRechner(20);
+praxisRechner(80);
+lernRechner();
+console.log(parameter);
