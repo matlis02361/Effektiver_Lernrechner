3aa4444ca9f7245064246ca21f22c2cdea90cdf1
Mateusz <matlis02361@gmail.com>
2022-09-22 14:21:19 +0200

    test branch

--- index.js
+++ index.js
@@ -17,40 +17,104 @@ lernRechner = (zeit, Tätigkeit) => {
 
   for (let index = 0; index < zeit; index += 5) {
     if (index >= 25) {
-      parameter.konzentration -= 0.01 * parameter.konzentration;
+      parameter.konzentration -= 0.005 * parameter.konzentration;
     }
     if (index >= 25 && index <= 30) {
-      parameter.konzentration -= 0.015 * parameter.konzentration;
+      parameter.konzentration -= 0.01 * parameter.konzentration;
     }
     if (index > 30 && index <= 45) {
-      parameter.konzentration -= 0.02 * parameter.konzentration;
+      parameter.konzentration -= 0.015 * parameter.konzentration;
     }
     if (index > 45 && parameter.konzentration >= 0) {
-      parameter.konzentration -= 0.03 * parameter.konzentration;
+      parameter.konzentration -= 0.02 * parameter.konzentration;
     }
   }
+  parameter.konzentration = Math.round(parameter.konzentration);
   /* ----------------------------------------------------------- */
   let LernnZeit = parameter.theorieZeit + parameter.praxisZeit;
-  parameter.praxisToLernnZeit = parameter.praxisZeit / LernnZeit;
+  parameter.praxisToLernnZeit =
+    Math.round((parameter.praxisZeit / LernnZeit) * 100) / 100;
 
-  parameter.lernEffizienz =
-    100 - Math.abs(0.8 - parameter.praxisToLernnZeit) * parameter.konzentration;
+  parameter.lernEffizienz = Math.round(
+    (1 - Math.abs(0.8 - parameter.praxisToLernnZeit)) * parameter.konzentration
+  );
 };
 
 /* ................... Pause....................................... */
 pauseRechner = (zeit) => {
-  pause += zeit;
-  if (zeit >= 5 && zeit <= 7) {
-    parameter.konzentration += 50;
-  } else if (zeit > 7) {
-    for (let index = 7; index < zeit; index += 5) {
-      parameter.konzentration += 1;
-      lernEffizienz -= 1;
+  parameter.pause += zeit;
+  for (let index = 0; index < zeit; index++) {
+    if (index < 5) {
+      parameter.konzentration += 0.1 * (100 - parameter.konzentration);
+    }
+    if (index >= 5 && index <= 7) {
+      parameter.konzentration += 0.5 * (100 - parameter.konzentration);
+    }
+    if (index > 7) {
+      parameter.konzentration += 0.01 * (100 - parameter.konzentration);
     }
   }
+  parameter.konzentration = Math.round(parameter.konzentration);
 };
+lernRechner(20,"Theorie")
+pauseRechner(10)
+lernRechner(20,"Theorie")
 
-lernRechner(45, "Theorie");
-console.log(parameter);
-lernRechner(45, "Praxis");
-console.log(parameter);
+/* ,`,`,`,`,`,`,`,`,`,`,`,`INPUT,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,`,` */
+
+document.write(`<select class="form-select" aria-label="Default select example">
+<option selected>Open this select menu</option>
+<option value="1">One</option>
+<option value="2">Two</option>
+<option value="3">Three</option>
+</select>`);
+
+/* :::::::::::::TABELE:::::::::::::::::::::::: */
+
+document.write(`<table class="table">
+<thead>
+  <tr>
+    <th scope="col">#</th>
+    <th scope="col">Parameter</th>
+    <th scope="col">Ergebnis</th>
+ 
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <th scope="row">1</th>
+    <td>Praxis</td>
+    <td>${parameter.praxisZeit} min</td>
+
+  </tr>
+  <tr>
+    <th scope="row">2</th>
+    <td>Theorie</td>
+    <td>${parameter.theorieZeit} min</td>
+  
+  </tr>
+  <tr>
+    <th scope="row">3</th>
+    <td>konzentration</td>
+    <td>${parameter.konzentration}%</td>
+  </tr>
+  <tr>
+    <th scope="row">4</th>
+    <td>Lerneffizienz</td>
+    <td>${parameter.lernEffizienz}%</td>
+  
+  </tr>
+  <tr>
+    <th scope="row">5</th>
+    <td>Pause</td>
+    <td>${parameter.pause} min</td>
+  
+  </tr>
+  <tr>
+    <th scope="row">6</th>
+    <td>Anteil der Theorie an der gesamten Lernzeit</td>
+    <td>${parameter.praxisToLernnZeit * 100}%</td>
+  
+  </tr>
+</tbody>
+</table>`);
